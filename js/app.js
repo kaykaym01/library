fee2990677f1b69a51f29b0c881ca723f73d6e08
kaykaym01 <kaymoore01@gmail.com>
2022-06-28 11:05:34 -0400

    Add function to add a new book to the library

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 /**
  * Represents a book.
  * @constructor
@@ -11,4 +13,16 @@ function Book(title, author, numPages, haveRead) {
     this.author = author;
     this.numPages = numPages;
     this.haveRead = haveRead;
+}
+
+/**
+ * Adds a new book to the library
+ * @param {string} title - Title of the book
+ * @param {string} author - Author of the book
+ * @param {number} numPages - Number of pages in the book
+ * @param {boolean} haveRead - Has the user read this book
+ */
+function addBookToLibrary(title, author, numPages, haveRead){
+    let newBook = new Book(title, author, numPages, haveRead);
+    myLibrary.push(newBook);
 }
