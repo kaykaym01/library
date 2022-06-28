6c767583ac8b870e986a04330b1001c03db06ca7
kaykaym01 <kaymoore01@gmail.com>
2022-06-28 11:01:45 -0400

    Add jsdoc comments to Book constructor

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
-function Book(title, author, numPages, haveRead){
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} title - Title of the book
+ * @param {string} author - Author of the book
+ * @param {number} numPages - Number of pages in the book
+ * @param {boolean} haveRead - Has the user read this book
+ */
+function Book(title, author, numPages, haveRead) {
     this.title = title;
     this.author = author;
     this.numPages = numPages;
