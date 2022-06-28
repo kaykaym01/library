9c83c36070e285f70442079c8a165e8481a6ec0f
kaykaym01 <kaymoore01@gmail.com>
2022-06-28 11:40:16 -0400

    Add methods to display each book from myLibrary in table

--- js/app.js
+++ js/app.js
@@ -26,3 +26,39 @@ function addBookToLibrary(title, author, numPages, haveRead){
     let newBook = new Book(title, author, numPages, haveRead);
     myLibrary.push(newBook);
 }
+
+/**
+ * Adds a book to the library display
+ * @param {Book} book 
+ */
+function addBooktoDisplay(book){
+    libraryTable = document.querySelector(".library-table");
+    newRow = document.createElement("tr");
+
+    bookTitle = document.createElement("td");
+    bookTitle.textContent = book.title;
+    newRow.append(bookTitle);
+
+    bookAuthor = document.createElement("td");
+    bookAuthor.textContent = book.author;
+    newRow.append(bookAuthor);
+
+    bookNumPages = document.createElement("td");
+    bookNumPages.textContent = book.numPages;
+    newRow.append(bookNumPages);
+
+    bookReadStatus = document.createElement("td");
+    bookReadStatus.textContent = (book.haveRead) ? "Have Read" : "Have Not Read";
+    newRow.append(bookReadStatus);
+
+    libraryTable.append(newRow);
+}
+
+/**
+ * Displays all books in myLibrary in the library table
+ */
+function displayLibrary(){
+    myLibrary.forEach(book => {
+        addBooktoDisplay(book);
+    })
+}
